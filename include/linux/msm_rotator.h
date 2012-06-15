2861ec9aac94d52772a8d7273e1d68ec1bf3f08d
freexperia <jerpelea@gmail.com>
2012-06-15 22:02:06 +0300

    Revert "msm_rotator: Bulk update to 2.6.35"
    This reverts commit e81ec0eab4841268169330f766bb317f17534f77.
    
    Signed-off-by: nobodyAtall <tsogkas@ceid.upatras.gr>

--- include/linux/msm_rotator.h
+++ include/linux/msm_rotator.h
@@ -41,8 +41,6 @@
 #define MSM_ROTATOR_IOCTL_FINISH   \
 		_IOW(MSM_ROTATOR_IOCTL_MAGIC, 3, int)
 
-#define ROTATOR_VERSION_01	0xA5B4C301
-
 enum rotator_clk_type {
 	ROTATOR_AXI_CLK,
 	ROTATOR_PCLK,
@@ -64,9 +62,6 @@ struct msm_rotator_data_info {
 	int session_id;
 	struct msmfb_data src;
 	struct msmfb_data dst;
-	unsigned int version_key;
-	struct msmfb_data src_chroma;
-	struct msmfb_data dst_chroma;
 };
 
 struct msm_rot_clocks {
@@ -79,7 +74,6 @@ struct msm_rotator_platform_data {
 	unsigned int number_of_clocks;
 	unsigned int hardware_version_number;
 	struct msm_rot_clocks *rotator_clks;
-	const char *regulator_name;
 };
 #endif
 
